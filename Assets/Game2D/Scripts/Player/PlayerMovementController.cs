e35a686abbc2415f6dd1f39aff37c72381c661ba
Hellty <korsovm@gmail.com>
2025-05-20 21:05:51 +0300

    Improve movement

--- Assets/Game2D/Scripts/Player/PlayerMovementController.cs
+++ Assets/Game2D/Scripts/Player/PlayerMovementController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,6 +11,8 @@ namespace Game2D.Scripts.Player
     public class PlayerMovementController : MonoBehaviour, IDoubleJumpResetable
     {
         [SerializeField] private Transform visuals;
+        private Vector3 _visualsDefaultOffset;
+
         [Header("Movement")] public float moveSpeed;
 
         private float _moveDirectionX;
@@ -58,11 +61,12 @@ namespace Game2D.Scripts.Player
         private readonly int ON_WALL_ANIM_KEY = Animator.StringToHash("OnWall");
         private readonly int JUMP_ANIM_KEY = Animator.StringToHash("Jump");
         private readonly int RUN_ANIM_KEY = Animator.StringToHash("Run");
-        
+
         private void Awake()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             _animator = GetComponentInChildren<Animator>();
+            _visualsDefaultOffset = visuals.localPosition;
         }
 
         private void FixedUpdate()
@@ -72,7 +76,7 @@ namespace Game2D.Scripts.Player
                 _rigidbody2D.velocity = new Vector2(_moveDirectionX * moveSpeed, _rigidbody2D.velocity.y);
 
             _animator.SetBool(RUN_ANIM_KEY, _rigidbody2D.velocity.x != 0);
-            
+
             // Allow to jump higher on button hold
             if (_isJumping && Time.fixedTime - _jumpStartTime <= jumpAllowAdjustTime)
                 _rigidbody2D.AddForce(Vector2.up * jumpAdjustForce, ForceMode2D.Impulse);
@@ -207,12 +211,11 @@ namespace Game2D.Scripts.Player
             _rigidbody2D.gravityScale = 1f;
         }
 
-        private void OnCollisionEnter2D(Collision2D collision)
+        private void OnCollisionStay2D(Collision2D collision)
         {
             var collisionPoint = collision.collider.ClosestPoint(transform.position);
 
-            // If collision point is not underneath -> colliding with wall (+0.2f is threshold)
-            if (collisionPoint.y > (transform.position.y - transform.localScale.y / 2f) + 0.2f)
+            if (Mathf.Abs(collisionPoint.y - transform.position.y) < 0.02f)
             {
                 _wallIsOnRight = collisionPoint.x > transform.position.x;
                 _wallSlidingFrom = collision.gameObject;
@@ -222,7 +225,10 @@ namespace Game2D.Scripts.Player
         private void OnCollisionExit2D(Collision2D collision)
         {
             if (collision.gameObject.Equals(_wallSlidingFrom))
+            {
                 _wallSlidingFrom = null;
+                OnWallSlidingExit();
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
@@ -267,15 +273,17 @@ namespace Game2D.Scripts.Player
 
             _isOnWall = true;
             _animator.SetBool(ON_WALL_ANIM_KEY, _isOnWall);
-            visuals.Translate(new Vector3(_isFacingRight? wallSitAnimSpriteOffset : -wallSitAnimSpriteOffset, 0f, 0f)); 
+            visuals.Translate(new Vector3(_isFacingRight ? wallSitAnimSpriteOffset : -wallSitAnimSpriteOffset, 0f, 0f));
             _isLanding = false;
             hasDoubleJump = true;
 
             _rigidbody2D.gravityScale = 0;
+            _rigidbody2D.velocity = Vector2.zero;
+            _rigidbody2D.totalForce = Vector2.zero;
             _yLevelToMaintain = transform.position.y;
 
             // To make player move with object he is grounded on
-            gameObject.transform.SetParent(gameObjectSlidingOn.transform);
+            // gameObject.transform.SetParent(gameObjectSlidingOn.transform);
 
             _slidingCoroutine = StartCoroutine(SlidingCoroutine());
         }
@@ -302,14 +310,19 @@ namespace Game2D.Scripts.Player
         {
             Debug.Log("Wall sliding exit");
 
+            if (!_isOnWall)
+                return;
+
             _isOnWall = false;
             _animator.SetBool(ON_WALL_ANIM_KEY, _isOnWall);
-            visuals.Translate(new Vector3(_isFacingRight? -wallSitAnimSpriteOffset : wallSitAnimSpriteOffset, 0f, 0f)); 
+            visuals.localPosition = _visualsDefaultOffset;
             _lastTimeGrounded = Time.time;
 
             _rigidbody2D.gravityScale = 1;
+            _rigidbody2D.velocity = Vector2.zero;
+            _rigidbody2D.totalForce = Vector2.zero;
 
-            gameObject.transform.SetParent(null);
+            // gameObject.transform.SetParent(null);
 
             if (_slidingCoroutine != null)
                 StopCoroutine(_slidingCoroutine);
